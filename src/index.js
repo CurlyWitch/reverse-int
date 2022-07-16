1d1686a3dd784041a454f51479c3e3cf16789809
CurlyWitch <yuliya.skrygan@gmail.com>
2022-07-16 18:46:45 +0300

    feat: add implementation reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let stringN = String(n);
+  let splitStr = stringN.split("");
+  let reverseArray = splitStr.reverse();
+  let joinArray = reverseArray.join("");
+  let result = +joinArray;
+  return result;
 }
